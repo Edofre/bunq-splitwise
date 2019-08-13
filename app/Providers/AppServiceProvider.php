1ea61119be17beae1d4b5c253f59e564b74e707f
Edo Freriks <edofre@gmail.com>
2019-08-13 21:46:59 +0200

    Debugbar & ide helper installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,21 +4,25 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class AppServiceProvider
+ * @package App\Providers
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
      * @return void
      */
     public function register()
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
      * Bootstrap any application services.
-     *
      * @return void
      */
     public function boot()
