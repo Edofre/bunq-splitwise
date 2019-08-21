c9087aa82d1281176a50ba4c6dd56c4b93fe9a79
Edo Freriks <edofre@gmail.com>
2019-08-21 23:11:18 +0200

    Sync command, not working because of api context

---
+++ app/Console/Commands/Bunq/SyncPayments.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands\Bunq;
+
+use Illuminate\Console\Command;
+
+/**
+ * Class SyncPayments
+ * @package App\Console\Commands\Bunq
+ */
+class SyncPayments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'bunq:sync-payments {monetaryAccount}';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = 'Sync payments';
+
+    /**
+     * Create a new command instance.
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * @return mixed
+     */
+    public function handle()
+    {
+        \App\Jobs\Bunq\SyncPayments::dispatch($this->argument('monetaryAccount'));
+    }
+}
