c197a1d1ce5d998f36e34a7a8fecdcc598d445b6
Edo Freriks <edofre@gmail.com>
2019-09-07 16:59:33 +0200

    Fractal installation

---
+++ app/Transformers/MonetaryAccountTransformer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class MonetaryAccountTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform()
+    {
+        return [
+            //
+        ];
+    }
+}
