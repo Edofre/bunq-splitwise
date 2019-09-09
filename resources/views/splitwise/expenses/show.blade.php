8de7a9c5c61e0ac5e6e1194c64b416b0cea376f4
Edo Freriks <edofre@gmail.com>
2019-09-09 22:26:03 +0200

    Titles for all views

--- resources/views/splitwise/expenses/show.blade.php
+++ resources/views/splitwise/expenses/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('title')
+    {{ $expense->description }}
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
--- resources/views/splitwise/friends/show.blade.php
+++ resources/views/splitwise/friends/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('title')
+    {{ $friend->first_name }}
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
