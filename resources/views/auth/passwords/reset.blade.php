90fab6e1402622a0f96f9508a67874e9fa3db970
Edo Freriks <edofre@gmail.com>
2019-09-12 22:45:09 +0200

    Auth views styling

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-10">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
